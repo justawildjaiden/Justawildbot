5656b8cc86495ba3f6540cb5324cf039c86ee023
Jaiden <jaiden.kaptein@outlook.com>
2024-12-26 21:51:13 +0100

    readability changegs

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,12 +1,14 @@
 import discord
+from discord import DMChannel
+
 
 class Latency(discord.Cog): #makes a class for the cog that inherts from discord.Cog
     #cogs are used to add functions to the bot, like a module
 
     def __init__(self,bot): #this method is called when the bot is loaded
         self.bot = bot
-
     # this command responds with the latency of the bot
+
     @discord.slash_command(description=f'Sends the latency of the bot', name=f'ping')
     async def latency_cmd(self, ctx):
         ping = round(self.bot.latency * 1000)
